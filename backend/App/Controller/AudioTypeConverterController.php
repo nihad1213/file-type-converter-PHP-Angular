3015dcd08429411a70d1adf85f8a932ee8645550
nihad1213 <nihad.nemetli@gmail.com>
2025-12-19 21:57:27 +0400

    add: methods added to controller and enpoint created in index.php

--- backend/App/Controller/AudioTypeConverterController.php
+++ backend/App/Controller/AudioTypeConverterController.php
@@ -6,5 +6,23 @@ namespace App\Controller;
 
 class AudioTypeConverterController extends AbstractController
 {
-    
+    public function flacToMp3()
+    {
+
+    }
+
+    public function mp3ToFlac()
+    {
+
+    }
+
+    public function mp3ToWav()
+    {
+
+    }
+
+    public function wavToMp3()
+    {
+        
+    }
 }
--- backend/public/index.php
+++ backend/public/index.php
@@ -3,14 +3,18 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use App\Controller\AbstractController;
+use App\Controller\AudioTypeConverterController;
 use Src\Router;
 
 $router = new Router();
 
-// $router->get("/", [TestController::class, 'test']);
-
 $router->get("/", [AbstractController::class, 'initialize']);
 
+// Audio Types
+$router->post("/flac-to-mp3", [AudioTypeConverterController::class, 'flacToMp3']);
+$router->post("/mp3-to-flac", [AudioTypeConverterController::class, 'mp3ToFlac']);
+$router->post("/mp3-to-wav", [AudioTypeConverterController::class, 'mp3ToWav']);
+$router->post("/wav-to-mp3", [AudioTypeConverterController::class, 'wavToMp3']);
 
 
 $router->dispatch();
